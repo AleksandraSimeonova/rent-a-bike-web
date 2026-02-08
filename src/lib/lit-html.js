8004a5524e3443264991fea156f3866dc27c18f1
AleksandraSimeonova <209593041+AleksandraSimeonova@users.noreply.github.com>
2026-02-08 23:39:18 +0200

    Setup lite-server

---
+++ src/lib/lit-html.js
@@ -0,0 +1,15 @@
+import { render , html } from '../../node_modules/lit-html/lit-html.js';
+
+///
+///const rootElement = document.getElementById('root');
+///
+///function render(templateResult){
+///
+///    return baseRender(templateResult, rootElement);
+///}
+///
+
+export {
+    render,
+    html 
+}
