cb024ad42c069ee8f828fe88580954e05151d31c
AleksandraSimeonova <209593041+AleksandraSimeonova@users.noreply.github.com>
2026-02-09 09:37:07 +0200

    Firebase Init file

---
+++ src/config/firebaseInit.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB39FcMAzhojCXDn2rWzaPY1gZiyoJ0mms",
+  authDomain: "rent-a-bike-softuni1.firebaseapp.com",
+  projectId: "rent-a-bike-softuni1",
+  storageBucket: "rent-a-bike-softuni1.firebasestorage.app",
+  messagingSenderId: "585609971456",
+  appId: "1:585609971456:web:9cf7ad6f033c8d463615e5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
